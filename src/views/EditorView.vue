06eb8844d99bcf6f8e22f6bb4299ce792cfc36d7
Elvinsky <mikhnevichnik@gmail.com>
2023-05-01 17:46:23 +0300

    feat: add drawing onMouseEnter, shrinking bug fixed

--- src/views/EditorView.vue
+++ src/views/EditorView.vue
@@ -18,9 +18,9 @@ const username: Ref<string | null> = ref(
     ? JSON.parse(localStorage.getItem("user")!).name
     : null
 );
-
 const canvas: Ref<HTMLCanvasElement | null> = ref(null);
 const ctx: Ref<CanvasRenderingContext2D | null> = ref(null);
+const isDrawing: Ref<boolean> = ref(false);
 (async () => {
   await nextTick(); // Wait for the next tick to ensure the canvas element is mounted
   canvas.value = document.getElementById("canvas") as HTMLCanvasElement;
@@ -28,8 +28,41 @@ const ctx: Ref<CanvasRenderingContext2D | null> = ref(null);
     ctx.value = canvas.value.getContext("2d") as CanvasRenderingContext2D;
   }
 })().then(() => {
-  console.log(ctx.value);
+  if (canvas.value) {
+    canvas.value.addEventListener("mousedown", startDrawing);
+    canvas.value.addEventListener("mousemove", draw);
+    canvas.value.addEventListener("mouseup", stopDrawing);
+    canvas.value.addEventListener("mouseout", stopDrawing);
+  }
 });
+function startDrawing(event: MouseEvent) {
+  isDrawing.value = true;
+  draw(event);
+}
+
+function draw(event: MouseEvent) {
+  if (!isDrawing.value || !ctx.value || !canvas.value) return;
+
+  // Calculate the offset between the canvas and the window
+  const offsetX = canvas.value.offsetLeft;
+  const offsetY = canvas.value.offsetTop;
+
+  // Calculate the mouse position relative to the canvas
+  const x = event.clientX - offsetX;
+  const y = event.clientY - offsetY;
+
+  ctx.value.lineWidth = 5;
+  ctx.value.lineCap = "round";
+  ctx.value.strokeStyle = "black";
+  ctx.value.beginPath();
+  ctx.value.moveTo(x, y);
+  ctx.value.lineTo(x, y);
+  ctx.value.stroke();
+}
+
+function stopDrawing() {
+  isDrawing.value = false;
+}
 </script>
 
 <style scoped>
