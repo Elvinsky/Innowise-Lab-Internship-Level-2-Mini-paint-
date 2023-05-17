94e2a514307db67d760f939bb7246c8deeda90c8
Elvinsky <mikhnevichnik@gmail.com>
2023-05-17 16:41:37 +0300

    feat&refactor: added firestore for users and field for images

---
+++ src/scripts/dbScripts/crudApi.ts
@@ -0,0 +1,36 @@
+// eslint-disable-next-line max-len
+import {
+  collection,
+  getDocs,
+  deleteDoc,
+  doc,
+  updateDoc,
+  setDoc,
+  arrayUnion,
+} from "firebase/firestore/lite";
+import { db } from "@/firebase";
+export async function getItems(path: string) {
+  const itemsCol = collection(db, path);
+  const itemSnapshot = await getDocs(itemsCol);
+  const itemList = itemSnapshot.docs.map((doc) => doc.data());
+  return itemList;
+}
+export async function setItem(item: string, data: any, id: string) {
+  const docRef = doc(db, item, id);
+  await setDoc(docRef, { ...data, id });
+}
+
+export async function updateItem(
+  item: string,
+  itemId: string,
+  element: string
+) {
+  const itemRef = doc(db, item, itemId.toString());
+  await updateDoc(itemRef, { images: arrayUnion(element) });
+}
+
+export async function deleteItem(collName: string, itemId: string) {
+  const itemRef = doc(db, collName, itemId);
+  await deleteDoc(itemRef);
+  console.log("Document deleted with id ", itemId);
+}
